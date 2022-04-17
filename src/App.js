65e309906a5b3080938011f183c67977ecc6924a
Nizar12t <99197481+Nizar12t@users.noreply.github.com>
2022-04-17 20:42:00 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Navbars/Navbar"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+     <BrowserRouter >
+        <NavLink
+          to={"/"}
+          className={({isActive}) => (isActive ? "link active" : "link")}
         >
-          Learn React
-        </a>
-      </header>
+          Home
+        </NavLink>
+
+        <NavLink
+          className={({isActive}) => (isActive ? "link active" : "link")}
+          to={"/about"}
+        >
+          About
+        </NavLink>
+     
+
+        <Routes>
+          <Route exact path="/" index={0} element={<Homme />} />
+          <Route exact path="/about" index={1} element={<About />} />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Navbars/Navbar.js
@@ -0,0 +1,21 @@
+
+import React ,{useState,useEffect}from 'react'
+import "./Navbar.css"
+
+export default function () {
+  return (
+    <nav>
+        <ul className='liste'>
+            <NavLink className='items'>aaa</NavLink>
+            <NavLink className='items'>bbb</NavLink>
+            < NavLink className='items'>ccc</NavLink>
+            <NavLink className='items'>ddd</NavLink>
+            <NavLink className='items'>eee</NavLink>
+
+
+        </ul>
+
+
+    </nav>
+  )
+}
